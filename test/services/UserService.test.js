b873038d9222178da8219137f95e002736348072
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-28 02:36:22 -0500

    Añadiendo requerimiento 2

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -3,11 +3,20 @@ const UserService = require('./../../app/services/UserService')
 describe("Test for UserService", () => {
     
     test("1. Create a new user using the UserService", () => {
-        const user = UserService.create(1, "carlogilmar", "Carlo")
-        expect(user.username).toBe("carlogilmar")
-        expect(user.name).toBe("Carlo")
+        const user = UserService.create(1, "rubenotoniel", "Oto")
+        expect(user.username).toBe("rubenotoniel")
+        expect(user.name).toBe("Oto")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
 
 })
+
+    test("2. Get all user data in a list", () => {
+        const user = UserService.create(1,"rubenotoniel","Oto")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("rubenotoniel")
+        expect(userInfoInList[2]).toBe("Oto")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
