0e400bb99d237f88a08e368726d8428426a565cb
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-29 21:24:48 -0500

    Agregando requerimiento 3, crear new user con payload

--- app/view/UserView.js
+++ app/view/UserView.js
@@ -7,6 +7,10 @@ class UserView {
         if (payload === null) {
             return objMessage = {error:"payload no existe"}
         }
+
+        else if(typeof payload.username !== 'string' || typeof payload.name !=='string' || typeof payload.id !=='number'){
+            return objMessage = {error:"necesitan un valor valido"}
+        }
     }
 }
 
--- test/view/UserView.test.js
+++ test/view/UserView.test.js
@@ -10,4 +10,10 @@ describe("Tests for UserView", () => {
 
     })
 
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan un valor valido/)
+    })
+
 })
